3014e150b9e066d28e49b9e86a01ab9c09e9dff2
Amresh Yadav <amreshky998@gmail.com>
2024-11-19 22:25:58 +0530

    mongodb-connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
+const ConnectDb = require("./utils/db.js");
+
+// env variables access
+require("dotenv").config();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // This line of code adds Express middleware that parses incoming request bodies with JSON payloads. It's important to place this before any routes that need to handle JSON data in the request body. This middleware is responsible for parsing JSON data from requests, and it should be applied at the beginning of your middleware stack to ensure it's available for all subsequent route handlers.
+
 
 // use for mounting the router
 app.use("/api/auth", router);
@@ -17,8 +23,11 @@ app.use("/api/auth", router);
 //     res.status(200).send("Welcome to registration page");
 // });
 
-// to start the server
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running at port: ${PORT}`);
+
+//database connection to mongodb and server run
+ConnectDb().then(()=>{
+  app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+  })
 });
+
---
+++ server/utils/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const URI = process.env.MONGODB_URI;
+
+console.log(URI);
+const ConnectDb = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log("Database Connection Successful");
+    } catch (error) {
+        console.log("Database connection failed");
+        process.exit(0); // just exit is db not connected
+    }
+}
+
+
+module.exports = ConnectDb;
